9ce1a72a59a12736e52fb23e70dfe0b1ce0afb16
George Gilfond <gsgilfond@gmail.com>
2022-07-17 18:24:21 -0400

    complete exercises 1-7

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...values) {
+    for (let i = 0; i < array.length; i++) {
+        if (values.includes(array[i])) {
+            array.splice(i, 1);
+            i--;
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,30 @@
-const sumAll = function() {
+const sumAll = function(limit1, limit2) {
+    let lowerLimit = 0;
+    let upperLimit = 0;
+    
+    if (limit1 < 0 || limit2 < 0) {
+        return "ERROR";
+    }
 
+    if (!Number.isInteger(limit1) || !Number.isInteger(limit2)) {
+        return "ERROR";
+    }
+
+    if (limit1 < limit2) {
+        lowerLimit = limit1;
+        upperLimit = limit2;
+    }
+    else {
+        lowerLimit = limit2;
+        upperLimit = limit1;
+    }
+
+    let sum = 0;
+
+    for (let i = lowerLimit; i <= upperLimit; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
